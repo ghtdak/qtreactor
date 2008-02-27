23fbf9b301747e34893a993cfdc49121f67d31c1
glenn tarbox <tarbox@ghtmyth.hq.tarbox.org>
2008-02-27 11:59:28 -0800

    fix admin

--- qtball.sh
+++ qtball.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git gc
 cd ..
-tar cvjf /home/tarbox/Desktop/qtreactor.tar.bz2 --exclude=.git --exclude='*pyc' qtreactor
+tar cvjf /home/tarbox/Desktop/qtreactor.tar.bz2 --exclude='.*' --exclude='*pyc' qtreactor
