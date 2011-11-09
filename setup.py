8b6eb31c9ae36a6e540358234451523a492d41ed
nessita <nessita@ernst.(none)>
2011-11-09 16:01:09 -0300

    - Adding distutils' setup.py so tarballs can be released and packaged.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# See LICENSE file for copyright and license details.
+
+"""Setup.py: build, distribute, clean."""
+
+import os
+import sys
+
+from distutils import log
+from distutils.core import setup
+from glob import glob
+
+
+setup(
+    name='qt4reactor',
+    version='1.0',
+    license='MIT',
+    author='Glenn H. Tarbox',
+    author_email='glenn@tarbox.org',
+    description='Twisted Qt4 Integration',
+    long_description='Provides support for Twisted to be driven by the ' \
+                     'Qt mainloop.',
+    url='https://github.com/ghtdak/qtreactor',
+    scripts=glob("./bin/*"),
+    py_modules=['qt4reactor', 'gtrial'],
+)
